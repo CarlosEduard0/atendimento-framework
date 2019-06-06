6b7d620c40bdd30c335bba055ca139cd90ece19c
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-06-06 14:05:04 -0300

    Adiciona algoritmo de realocar senhas no guichê de cartório

--- src/main/java/br/ufrn/imd/atendimentoframwork/service/CartorioGuicheService.java
+++ src/main/java/br/ufrn/imd/atendimentoframwork/service/CartorioGuicheService.java
@@ -4,8 +4,8 @@ import br.ufrn.imd.atendimentoframwork.model.Guiche;
 import br.ufrn.imd.atendimentoframwork.model.Senha;
 import br.ufrn.imd.atendimentoframwork.model.TipoServico;
 import br.ufrn.imd.atendimentoframwork.repository.GuicheRepository;
+import br.ufrn.imd.atendimentoframwork.repository.SenhaRepository;
 import br.ufrn.imd.atendimentoframwork.service.interfaces.GuicheService;
-import br.ufrn.imd.atendimentoframwork.service.interfaces.SenhaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +14,27 @@ import java.util.List;
 
 @Service
 public class CartorioGuicheService extends GuicheService {
+    private final SenhaRepository senhaRepository;
+
     @Autowired
-    public CartorioGuicheService(GuicheRepository guicheRepository) {
+    public CartorioGuicheService(GuicheRepository guicheRepository, SenhaRepository senhaRepository) {
         super(guicheRepository);
+        this.senhaRepository = senhaRepository;
     }
 
     @Override
     protected void realocarSenhasAoAtivar(Guiche guiche) {
-        // TODO
+        List<Guiche> guichesAtivos = guicheRepository.findByAtivoTrue();
+        if(!guichesAtivos.isEmpty()) {
+            List<Senha> senhas = new ArrayList<>();
+            for(Guiche g : guichesAtivos) {
+                senhas.addAll(g.getSenhas().subList(g.getSenhas().size() - (int) (g.getSenhas().size() * 0.2), g.getSenhas().size()));
+            }
+            for(Senha s : senhas) {
+                s.setGuiche(guiche);
+            }
+            senhaRepository.saveAll(senhas);
+        }
     }
 
     @Override
